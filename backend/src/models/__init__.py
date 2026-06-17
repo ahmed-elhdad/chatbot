9372410c95671bf103e040174d7754360c69f4d9
Ahmed elhaddad <ahalhdad2023@gmail.com>
2026-06-17 22:17:39 +0300

    Refactor imports in __init__.py and remove unused db_schemas module

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,2 +1,5 @@
 from .enums.ResponseEnum import ResponseSignal
 from .enums.DataBaseEnum import DataBaseEnum
+from .BaseDataModel import BaseDataModel
+from .ConversationModel import ConversationModel
+from .db_shcemas.conversation import Conversation
