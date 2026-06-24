5e112c058668008e0246ad3d213b67a473614b31
Ahmed elhaddad <ahalhdad2023@gmail.com>
2026-06-24 19:25:15 +0300

    Edit __init__.py

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -2,5 +2,5 @@ from .enums.ResponseEnum import ResponseSignal
 from .enums.ConversationEnum import ConversationEnum
 from .BaseDataModel import BaseDataModel
 from .ConversationModel import ConversationModel
-from .db_shcemas.conversation import ConversationModel
+from .db_shcemas.conversation import Conversation
 from .enums.AuthEnum import AuthEnum
